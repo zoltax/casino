f68df4104d20a52230e3c354bd9aa9732f440165
Filip Kulig <filip.kulig@performancehorizon.com>
2016-04-17 01:54:17 +0100

    Complete opening time repository.

---
+++ src/repository/OpeningTime.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace CC\Repository;
+
+
+class OpeningTime implements RepositoryInterface {
+
+	private $gateway = NULL;
+
+	public function __construct($gateway)
+	{
+		$this->gateway = $gateway;
+	}
+
+	public function getById($id)
+	{
+		$data =  $this->gateway->getById($id);
+		$entity = (new \CC\Factory\OpeningTime())->createFromData($data);
+		return $entity;
+
+	}
+
+	public function persist($data)
+	{
+		$data = $this->gateway->persist($data);
+		$entity = (new \CC\Factory\OpeningTime())->createFromData($data);
+		return $entity;
+	}
+
+	public function delete($id)
+	{
+		$data = $this->gateway->delete($id);
+		$entity = (new \CC\Factory\OpeningTime())->createFromData($data);
+		return $entity;
+	}
+
+	public function getByCasinoId($id)
+	{
+		$data = $this->gateway->getByCasinoId($id);
+		$factory = new \CC\Factory\OpeningTime();
+
+		$openingHours = [];
+
+		foreach ( $data as $openingHour )
+		{
+			$entity = $factory->createFromData($openingHour);
+			$openingHours[] = $entity;
+		}
+
+		return $openingHours;
+	}
+}
---
+++ tests/repository/OneningTimeRepositoryTest.php
@@ -0,0 +1,110 @@
+<?php
+
+class OpeningTimeRepositoryTest extends PHPUnit_Framework_TestCase {
+
+	public function testClass()
+	{
+		$openingTimeRepository = new \CC\Repository\OpeningTime('');
+		$this->assertEquals('CC\Repository\OpeningTime',get_class($openingTimeRepository));
+
+		// class implements the interface, so we need to test for methods
+
+		$this->assertTrue(method_exists($openingTimeRepository,'getById'));
+		$this->assertTrue(method_exists($openingTimeRepository,'persist'));
+		$this->assertTrue(method_exists($openingTimeRepository,'delete'));
+	}
+
+
+	public function testAddGateway()
+	{
+		$gateway = Mockery::mock()->
+			shouldReceive('getById')->andReturn([])->mock();
+
+		$openingTimeRepository = new \CC\Repository\OpeningTime($gateway);
+		$this->assertTrue(property_exists($openingTimeRepository,'gateway'));
+	}
+
+
+	public function testGetById()
+	{
+
+		$data = [
+			'id' => 1
+ 		];
+
+		$openingTimeEntity = (new \CC\Factory\OpeningTime())->createFromData($data);
+
+		$gateway = Mockery::mock()->
+			shouldReceive('getById')->andReturn($openingTimeEntity->asArray())->mock();
+
+		$openingTimeRepository = new \CC\Repository\OpeningTime($gateway);
+		$returnedEntity = $openingTimeRepository->getById(1);
+
+		$this->assertEquals($openingTimeEntity, $returnedEntity);
+		$this->assertNotNull($returnedEntity->getId());
+	}
+
+
+	public function testPersist()
+	{
+		$data = [];
+
+		$openingTimeEntity = (new \CC\Factory\OpeningTime())->createFromData($data);
+
+		$gateway = Mockery::mock()->
+		shouldReceive('persist')->andReturn($openingTimeEntity->asArray())->mock();
+
+		$openingTimeRepository = new \CC\Repository\OpeningTime($gateway);
+		$returnedEntity = $openingTimeRepository->persist($data);
+
+		$this->assertEquals($openingTimeEntity, $returnedEntity);
+	}
+
+	public function testDelete()
+	{
+
+		$data = [
+			'id' => NULL
+		];
+
+		$openingTimeEntity = (new \CC\Factory\OpeningTime())->createFromData($data);
+
+		$gateway = Mockery::mock()->
+			shouldReceive('delete')->andReturn($openingTimeEntity->asArray())->mock();
+
+		$openingTimeRepository = new \CC\Repository\OpeningTime($gateway);
+		$returnedEntity = $openingTimeRepository->delete(1);
+
+		$this->assertNull($returnedEntity->getId());
+
+
+	}
+
+	public function testGetOpeningTimeByCasinoId()
+	{
+		$data = [
+			'id'            => 1,
+			'day'           => 'Monday',
+			'opening_time'  => '07:00',
+			'closing_time'  => '19:00'
+		];
+
+		$openingTimeEntity = (new \CC\Factory\OpeningTime())->createFromData($data);
+
+		$gateway = Mockery::mock()->
+		shouldReceive('getByCasinoId')->andReturn([$openingTimeEntity->asArray()])->mock();
+
+		$openingTimeRepository = new \CC\Repository\OpeningTime($gateway);
+		$returnedEntities = $openingTimeRepository->getByCasinoId(1);
+
+		$this->assertTrue(is_array($returnedEntities));
+
+		$entity = array_pop($returnedEntities);
+
+		$this->assertEquals($openingTimeEntity, $entity);
+	}
+
+
+
+
+}
