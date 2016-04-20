2e9fe13e57dfaf2d1aae1f37f0e6234dfd932183
Filip Kulig <filip.kulig@performancehorizon.com>
2016-04-20 23:10:29 +0100

    Casino service. getById

---
+++ casino/app/Gateway/GeoLocalisation.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Gateway;
+
+
+use Illuminate\Support\Facades\DB;
+
+class GeoLocalisation {
+
+	public function getLocalisationByPostCode($postCode)
+	{
+
+		$response = file_get_contents("http://uk-postcodes.com/postcode/$postCode.json");
+
+		$data = json_decode($response);
+
+		$lat = $data->geo->lat;
+		$long = $data->geo->lng;
+
+		return [
+			'latitude' => $lat,
+			'longitude' => $long
+		];
+
+	}
+
+
+	public function getNearest($data)
+	{
+
+		return DB::select('SELECT id,name,latitude, longitude, SQRT(
+			POW(69.1 * (latitude - '.$data['latitude'].'), 2) +
+			POW(69.1 * ('.$data['longitude'].' - longitude) * COS(latitude / 57.3), 2)) AS distance
+			FROM casino HAVING distance < 2025 ORDER BY distance;');
+
+	}
+
+}
